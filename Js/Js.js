eb294f8c09286db8cb86032adda293e4ce010854
Lucas Raab <lurac3d@student.oaisd.org>
2026-03-24 09:26:01 -0400

    created the basic js will work on more later

---
+++ Js/Js.js
@@ -0,0 +1,15 @@
+// const sentinel = document.getElementById("sentinel");
+
+// const observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       console.log("Sentinel is visible!");
+//       // Append new content here
+//     }
+//   });
+// }, {
+//   root: null,      // null = viewport
+//   threshold: 1.0   // 1.0 = fully visible
+// });
+
+// observer.observe(sentinel);
